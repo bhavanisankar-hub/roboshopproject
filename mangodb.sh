1f13158557af4a9bc4c45f002977cf2fa2b40c0f
Bhavanisankar <shankar4137@gmail.com>
2023-06-23 20:22:52 +0530

    mango

--- mangodb.sh
+++ mangodb.sh
@@ -8,6 +8,7 @@ echo -e "\e[31mupdate listen address\e[0m"
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
+
 echo -e "\e[31mRestarting\e[0m"
 systemctl enable mongod  &>>/tmp/roboshop.log
 systemctl res  tart mongod  &>>/tmp/roboshop.log
