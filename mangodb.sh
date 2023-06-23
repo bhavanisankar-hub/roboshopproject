6330e5a77f7fe8fbdbfbda7f86c5b5e90ad5d6ed
Bhavanisankar <shankar4137@gmail.com>
2023-06-23 15:22:25 +0530

    mango

--- mangodb.sh
+++ mangodb.sh
@@ -1,8 +1,8 @@
-cp mangodb.repo /etc/yum.repos.d/mangodb.repo  &>>/temp/roboshop.log
+cp mangodb.repo /etc/yum.repos.d/mangodb.repo  &>>/tmp/roboshop.log
 
-yum install mongodb-org -y  &>>/temp/roboshop.log
+yum install mongodb-org -y  &>>/tmp/roboshop.log
 
 # update listen address from 127.0.0.1 to 0.0.0.0
 
-systemctl enable mongod  &>>/temp/roboshop.log
-systemctl restart mongod  &>>/temp/roboshop.log
+systemctl enable mongod  &>>/tmp/roboshop.log
+systemctl restart mongod  &>>/tmp/roboshop.log
