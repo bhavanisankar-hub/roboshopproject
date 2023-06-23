65d8cbc15a287a0108be50335646ff913fa0b280
Bhavanisankar <shankar4137@gmail.com>
2023-06-23 15:16:44 +0530

    mango

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -1,19 +1,19 @@
 echo -e "\e[31mInstalling nginx server\e[0m"
-yum install nginx -y  &>>/temp/roboshop.log
+yum install nginx -y  &>>/temp/roboshopproject.log
 
 
 echo -e "\e[31mRemoving old content\e[0m"
-rm -rf /usr/share/nginx/html/*  &>>/temp/roboshop.log
+rm -rf /usr/share/nginx/html/*  &>>/temp/roboshoproject.log
 
 echo -e "\e[31mDownloading Frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/temp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/temp/roboshopproject.log
 
 echo -e "\e[31mExtracting frontend content\e[0m"
-cd /usr/share/nginx/html  &>>/temp/roboshop.log
-unzip /tmp/frontend.zip  &>>/temp/roboshop.log
+cd /usr/share/nginx/html  &>>/temp/roboshopproject.log
+unzip /tmp/frontend.zip  &>>/temp/roboshopproject.log
 
 #copy the configuration file
 
 echo -e "\e[31mstarting nginx server\e[0m"
-systemctl enable nginx  &>>/temp/roboshop.log
-systemctl restart nginx  &>>/temp/roboshop.log
+systemctl enable nginx  &>>/temp/roboshopproject.log
+systemctl restart nginx  &>>/temp/roboshopproject.log
